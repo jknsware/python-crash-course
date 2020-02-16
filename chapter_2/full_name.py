73b7a02c090765bfc2d8e455e2abf27538a54ce1
Jason Ware <jason@jknsware.com>
2020-02-16 15:06:31 -0600

    Add chapter 2

---
+++ chapter_2/full_name.py
@@ -0,0 +1,40 @@
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print()
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print()
+
+name = "eric"
+print(f"Hello {name.title()}, would you like to learn some python today?")
+print()
+
+famous_person = "carl sagan"
+message = 'once said, "The brain is like a muscle. When it is in use we feel very good. Understanding is joyous."'
+print(f"{famous_person.title()} {message}")
+print()
+
+# Strip whitespace
+name = "\n\teric "
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+print ()
+
+# Math
+print(5+3)
+print(16-8)
+print(4*2)
+print(32/4)
+print()
+
+# Favorite number
+fav_number = 7
+print(f"My favorite number is {fav_number}.")
+print()
