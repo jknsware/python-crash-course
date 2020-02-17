ea3e75a7f74004b58576f49656e488dd920964a6
Jason Ware <jason@jknsware.com>
2020-02-16 21:40:31 -0600

    Add chapter_4

---
+++ chapter_4/numbers.py
@@ -0,0 +1,36 @@
+# 4.3
+numbers = list(range(1, 21))
+for number in numbers:
+  print(number)
+print("\n")
+
+# 4.4
+# millions = list(range(1, 1000000))
+# for number in millions:
+#   print(number)
+# print("\n")
+
+
+# 4.5
+millions = list(range(1, 10000001))
+print(min(millions))
+print(max(millions))
+print(sum(millions))
+print("\n")
+
+# 4.6
+odds = list(range(1, 20, 2))
+for odd in odds:
+  print(odd)
+print("\n")
+
+# 4.7
+threes = list(range(3, 31, 3))
+for three in threes:
+  print(three)
+print("\n")
+
+# 4.8
+cubes = []
+for number in range(1, 11):
+  number
---
+++ chapter_4/pizza.py
@@ -0,0 +1,18 @@
+# 4.1
+pizzas = ['pepperoni', 'hawaiian', 'cheese']
+for pizza in pizzas:
+  print(pizza)
+print()
+
+for pizza in pizzas:
+  print(f"I like {pizza} pizza.")
+
+print("I really like pizza.")
+print("\n")
+
+# 4.2
+animals = ['dog', 'cat', 'monkey']
+for animal in animals:
+  print(f"A {animal} would make a really good pet.")
+print(f"These would make great pets.")
+print("\n")
